38e04048805125e4bab169a1aaa7529c0bbabf34
Yiwei Gong <imwithye@gmail.com>
2023-12-01 14:04:30 +0000

    update deps and package deps into dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import legacy from '@rollup/plugin-legacy'
+import inject from '@rollup/plugin-inject'
+
 export default [
 	{
 		input: 'src/Potree.js',
@@ -7,8 +13,21 @@ export default [
 			format: 'umd',
 			name: 'Potree',
 			sourcemap: true,
-		}
-	},{
+		},
+		plugins: [
+			legacy({
+				'libs/other/BinaryHeap.js': 'BinaryHeap'
+			}),
+			inject({
+				proj4: 'proj4',
+				$: 'jquery',
+				jQuery: 'jquery',
+				BinaryHeap: path.resolve('libs/other/BinaryHeap.js')
+			}),
+			nodeResolve(),
+			commonjs()
+		]
+	}, {
 		input: 'src/workers/BinaryDecoderWorker.js',
 		output: {
 			file: 'build/potree/workers/BinaryDecoderWorker.js',
@@ -16,7 +35,7 @@ export default [
 			name: 'Potree',
 			sourcemap: false
 		}
-	},{
+	}, {
 		input: 'src/modules/loader/2.0/DecoderWorker.js',
 		output: {
 			file: 'build/potree/workers/2.0/DecoderWorker.js',
@@ -24,7 +43,7 @@ export default [
 			name: 'Potree',
 			sourcemap: false
 		}
-	},{
+	}, {
 		input: 'src/modules/loader/2.0/DecoderWorker_brotli.js',
 		output: {
 			file: 'build/potree/workers/2.0/DecoderWorker_brotli.js',
